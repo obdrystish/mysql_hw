8863a9808b718bab6bc5493560faf1e992851577
GG ALLIN <120009426+obdrystish@users.noreply.github.com>
2023-06-14 16:45:02 +0300

    Add files via upload

--- bookshelf_tasks.sql
+++ bookshelf_tasks.sql
@@ -15,14 +15,14 @@ LIMIT 5
 -- 3. 
 SELECT COUNT(shelves.title) as 'Количество книг' FROM shelves
 JOIN books ON shelves.id = books.shelves_id
-WHERE shelves.title LIKE '%кабин%'
+WHERE shelves.title = 'Полка в кабинете'
 ;
 -- 4. 
 SELECT books.title as 'Название', authors.name as 'Автор', books.year as 'Год', shelves.title 'Полка' FROM shelves
 JOIN books ON shelves.id = books.shelves_id
 JOIN authors_books ON books.id = authors_books.books_id
 JOIN authors ON authors_books.authors_id = authors.id
-WHERE shelves.title LIKE '%спал%'
+WHERE shelves.title = 'Полка в спальне'
 ;
 -- 5. 
 SELECT books.title as 'Книга', books.year 'Год', authors.name as 'Автор' FROM books
@@ -48,8 +48,11 @@ WHERE
 ;
 -- 8. 
 UPDATE books
-SET books.friends_id = 1
-WHERE id = 9
+JOIN authors_books ON authors_books.books_id = books.id
+JOIN authors ON authors_books.authors_id = authors.id
+SET books.friends_id = (SELECT friends.id FROM friends WHERE friends.name = 'Иванов Иван')
+WHERE authors.name = 'Данте Алигьери' AND books.title = 'Божественная комедия'
+and books.id > 0
 ;
 -- 9. 
 INSERT INTO books (books.title, books.year, books.shelves_id)
